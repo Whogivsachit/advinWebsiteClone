2b1e3accbff419484a0d890ebc15bbed3ab6b2cf
Whogivsachit <evanm6257@gmail.com>
2024-05-29 13:21:19 -0700

    Added sub dropdown for navbar ( This isnt a good way to do this )

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const dropdownBtn = document.getElementById('dropdownBtn');
     const dropdownMenu = document.getElementById('dropdownMenu');
+    const dropdownMenuSub = document.getElementById('dropdownMenuSub');
     
     dropdownBtn.addEventListener('click', () => {
         dropdownMenu.classList.toggle('hidden');
+        dropdownMenuSub.classList.toggle('hidden');
     });
 });
